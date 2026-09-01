8c7962b85f268bd1a5e8e77b0ad6f96b48bf6533
s4rub4ru <dayaricruz12@gmail.com>
2026-09-01 12:48:52 -0600

    Update TableToolbar.vue

--- catalogos_vue/daya/TableToolbar.vue
+++ catalogos_vue/daya/TableToolbar.vue
@@ -11,23 +11,32 @@ defineEmits(['update:search', 'update:estado']);
 </script>
 
 <template>
-  <div class="toolbar">
-    <input
-      class="search"
-      :value="search"
-      :placeholder="placeholder"
-      :data-testid="idPrefix + '-search'"
-      @input="$emit('update:search', $event.target.value)"
-    />
-    <select
-      v-if="estados.length"
-      class="search"
-      :value="estado"
-      :data-testid="idPrefix + '-estado-filter'"
-      @change="$emit('update:estado', $event.target.value)"
-    >
-      <option value="">Todos los estados</option>
-      <option v-for="e in estados" :key="e.value" :value="e.value">{{ e.label }}</option>
-    </select>
+  <div class="row g-3 mb-4">
+    <div :class="estados.length ? 'col-12 col-md-8' : 'col-12'">
+      <div class="input-group">
+        <span class="input-group-text bg-white border-end-0 text-muted">
+          <i class="bi bi-search"></i>
+        </span>
+        <input
+          type="text"
+          class="form-control border-start-0 ps-0"
+          :value="search"
+          :placeholder="placeholder"
+          :data-testid="idPrefix + '-search'"
+          @input="$emit('update:search', $event.target.value)"
+        />
+      </div>
+    </div>
+    <div v-if="estados.length" class="col-12 col-md-4">
+      <select
+        class="form-select"
+        :value="estado"
+        :data-testid="idPrefix + '-estado-filter'"
+        @change="$emit('update:estado', $event.target.value)"
+      >
+        <option value="">Todos los estados</option>
+        <option v-for="e in estados" :key="e.value" :value="e.value">{{ e.label }}</option>
+      </select>
+    </div>
   </div>
 </template>
