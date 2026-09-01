410184b10f4df36f9a143b0ddcdfcd0c0950b789
s4rub4ru <dayaricruz12@gmail.com>
2026-09-01 12:11:50 -0600

    Update ModalBase.vue

--- catalogos_vue/daya/ModalBase.vue
+++ catalogos_vue/daya/ModalBase.vue
@@ -10,22 +10,24 @@ defineEmits(['close', 'save']);
 </script>
 
 <template>
-  <div class="overlay" @click.self="$emit('close')">
-    <div class="modal-base" :class="{ lg: big }">
-      <div class="modal-head">
-        <h3>{{ title }}</h3>
-        <button class="btn btn-ghost" @click="$emit('close')"><i class="bi bi-x-lg"></i></button>
-      </div>
-      <div class="modal-body">
-        <slot />
-      </div>
-      <div v-if="!hideFooter" class="modal-foot">
-        <slot name="footer">
-          <button class="btn" @click="$emit('close')">Cancelar</button>
-          <button class="btn btn-brand" data-testid="modal-save-btn" @click="$emit('save')">
-            <i class="bi bi-check-lg"></i> Guardar
-          </button>
-        </slot>
+  <div class="modal fade show d-block bg-dark bg-opacity-50" tabindex="-1" @click.self="$emit('close')">
+    <div class="modal-dialog" :class="{ 'modal-lg': big }">
+      <div class="modal-content shadow">
+        <div class="modal-header">
+          <h5 class="modal-title fw-semibold">{{ title }}</h5>
+          <button type="button" class="btn-close" aria-label="Close" @click="$emit('close')"></button>
+        </div>
+        <div class="modal-body">
+          <slot />
+        </div>
+        <div v-if="!hideFooter" class="modal-footer">
+          <slot name="footer">
+            <button type="button" class="btn btn-outline-secondary" @click="$emit('close')">Cancelar</button>
+            <button type="button" class="btn btn-primary d-inline-flex align-items-center gap-1" data-testid="modal-save-btn" @click="$emit('save')">
+              <i class="bi bi-check-lg"></i> Guardar
+            </button>
+          </slot>
+        </div>
       </div>
     </div>
   </div>
